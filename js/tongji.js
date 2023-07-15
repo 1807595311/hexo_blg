7e93b7fa4640d11b20498d23c996be9d6a2d3e12
ycy <1807595311@qq.com>
2023-07-15 18:19:29 +0800

    Site updated: 2023-07-15 18:19:29

--- js/tongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?7fa308a9f634fe0a1d9280cee052399d";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
